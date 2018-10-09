5741af1814b61bc00a718faded9b7ede4e823109
Alexander Caroselli <Alexander.caroselli@allata.com>
2018-10-08 20:52:42 -0600

    Add project files.

---
+++ calorie-castle-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calorie_castle_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
